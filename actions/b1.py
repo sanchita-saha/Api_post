c478be10d604826c04d311d63e7a2c1c3c22dc5d
Sanchita Saha <sanchitas@icteas.com>
2019-08-08 13:31:52 +0530

    initial commit

--- actions/b1.py
+++ actions/b1.py
@@ -1,7 +1,7 @@
 import requests
 from st2common.runners.base_action import Action
-class MyEchoAction(Action):
+class book(Action):
 	book = {"Content-Type": "application/json", "Accept": "application/json" , 'book':'LOREM'}
 	resp = requests.post('http://fakerestapi.azurewebsites.net/api/Books', json=book)
 	if resp.status_code != 201:
-			print('Created book. ID: {}'.format(resp.json()["id"]))
+			print('Created book. ID: {}'.format(resp.json()[0]))
