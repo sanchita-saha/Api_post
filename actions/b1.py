3088608ea7ce6652f1629b3338d4df8f84c865c3
Sanchita Saha <sanchitas@icteas.com>
2019-08-08 15:12:10 +0530

    initial commit

--- actions/b1.py
+++ actions/b1.py
@@ -4,4 +4,4 @@ class book(Action):
 	book = {"Content-Type": "application/json", "Accept": "application/json" , 'book':'LOREM'}
 	resp = requests.post('http://fakerestapi.azurewebsites.net/api/Books', json=book)
 	if resp.status_code != 201:
-			print('Created book. ID: {}'.format(resp.json()[0]))
+			print(resp.json())
