ded7678d99278bd994b64bf6ce35bcc89ab89a6b
Sanchita Saha <sanchitas@icteas.com>
2019-08-08 13:15:41 +0530

    initial commit

--- actions/b1.py
+++ actions/b1.py
@@ -2,7 +2,7 @@ import requests
 from st2common.runners.base_action import Action
 class MyEchoAction(Action):
 	book = {"summary": "Take out trash", "description": "" }
-	resp = requests.post('https://todolist.example.com/tasks/', json=book)
+	resp = requests.post('http://fakerestapi.azurewebsites.net/api/Books', json=book)
 	if resp.status_code != 201:
 			raise ApiError('POST /tasks/ {}'.format(resp.status_code))
 			print('Created book. ID: {}'.format(resp.json()["id"]))
