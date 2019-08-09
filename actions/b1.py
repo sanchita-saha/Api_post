23b457867cc4994fe52fda2b5e0fca3190890c2a
Sanchita Saha <sanchitas@icteas.com>
2019-08-09 11:20:31 +0530

    initial commit

--- actions/b1.py
+++ actions/b1.py
@@ -1,8 +1,8 @@
 import requests
 from st2common.runners.base_action import Action
 class book(Action):
-	def run(self, book):
-		book = {"ID": "", "Title": "", "PageCount": "", "Excerpt": "", "PublishDate": ""}
+	def run(self,ID,Title,Description,PageCount,Excerpt,PublishDate):
+		book = {"ID": "ID", "Title": "Title", "Description": "string", "PageCount": "PageCount", "Excerpt": "Excerpt", "PublishDate": "PublishDate"}
 		resp = requests.post('http://fakerestapi.azurewebsites.net/api/Books', json=book)
 		if resp.status_code != 201:
 				print(resp.json())
